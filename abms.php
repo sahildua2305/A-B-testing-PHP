7e876a41d33f1465076c19de2857d1e026a4ca0a
Sahil Dua <sahildua2305@gmail.com>
2016-03-10 02:01:54 +0530

    Add variation method

--- abms.php
+++ abms.php
@@ -3,7 +3,7 @@
  * @Author: sahildua2305
  * @Date:   2016-03-10 01:05:47
  * @Last Modified by:   Sahil Dua
- * @Last Modified time: 2016-03-10 01:32:47
+ * @Last Modified time: 2016-03-10 01:55:48
  */
 
 
@@ -13,8 +13,11 @@ class abms
 	private $is_bot = FALSE;
 	private $test_duration;
 	private $test_start_timestamp;
+	private $variations = array();
+	private $adaptive = FALSE;
 	
-	function __construct ($name, $duration) {
+
+	function __construct ($name, $duration, $adaptive = FALSE) {
 
 		if($this->detect_bots == TRUE){
 			$bots = array('googlebot', 'msnbot', 'slurp', 'ask jeeves', 'crawl', 'ia_archiver', 'lycos');
@@ -31,6 +34,17 @@ class abms
 		$name = str_replace(' ', '_', $name);
 		$this->test_name = $name;
 		$this->test_duration = $duration;
+		$this->adaptive = $adaptive;
+	}
+
+
+	public function add_variation ($index, $value){
+		// add a new variation with given index and value to $this->variations
+		$a = array(
+			'index' => $index,
+			'value' => $value,
+		);
+		array_push($this->variations, $a);
 	}
 
 }
--- index.php
+++ index.php
@@ -3,9 +3,25 @@
  * @Author: sahildua2305
  * @Date:   2016-03-10 01:01:24
  * @Last Modified by:   Sahil Dua
- * @Last Modified time: 2016-03-10 01:26:39
+ * @Last Modified time: 2016-03-10 02:01:24
  */
 
 require_once('abms.php');
 
-$my_test = new abms('headline');
+$my_test = new abms('hyperlink text', 24*60*60, FALSE);
+
+my_test->add_variation(0, 'Sahil Dua');
+my_test->add_variation(1, 'Mansimar Kaur');
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+
+	</head>
+
+	<body>
+		
+	</body>
+</html>
