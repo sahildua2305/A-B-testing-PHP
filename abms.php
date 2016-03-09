fe53f54c407885ff5c7bc3da2a12aa9480f2061c
Sahil Dua <sahildua2305@gmail.com>
2016-03-10 02:43:47 +0530

    Find user segment for splitting

--- abms.php
+++ abms.php
@@ -3,7 +3,7 @@
  * @Author: sahildua2305
  * @Date:   2016-03-10 01:05:47
  * @Last Modified by:   Sahil Dua
- * @Last Modified time: 2016-03-10 01:55:48
+ * @Last Modified time: 2016-03-10 02:37:29
  */
 
 
@@ -15,6 +15,7 @@ class abms
 	private $test_start_timestamp;
 	private $variations = array();
 	private $adaptive = FALSE;
+	private $current_variation;
 	
 
 	function __construct ($name, $duration, $adaptive = FALSE) {
@@ -38,13 +39,32 @@ class abms
 	}
 
 
-	public function add_variation ($index, $value){
+	public function add_variation ($index, $value) {
 		// add a new variation with given index and value to $this->variations
 		$a = array(
 			'index' => $index,
 			'value' => $value,
 		);
 		array_push($this->variations, $a);
+	}
+
+
+	public function get_user_segment () {
+		if ($this->is_bot == TRUE) {
+			$this->current_variation = -1;
+			return $this->current_variation;
+		}
+
+		// check for which algorithm we're going to use for splitting the user traffic
+		if($this->adaptive == TRUE) {
+			// implement that fucking 90-10 logic
+		}
+		else {
+			// implement 50-50 logic (random)
+			$this->current_variation = rand(1, 100000) % 2;
+		}
+
+		return $current_variation;
 	}
 
 }
