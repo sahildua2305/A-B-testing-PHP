d2a2248e3d57ce4c0947f4ab38483d39a82c1e5f
MansimarKaur <mansimarkaur.mks@gmail.com>
2016-03-16 14:13:02 -0700

    Database.php added

--- includes/connection.inc.php
+++ includes/connection.inc.php
@@ -1,11 +1,11 @@
 <?php
 
-	$mysql_host = 'localhost';
-	$mysql_user = 'root';
-	$mysql_pass = '';
-	$mysql_data = 'ab_testing';
+	global $mysql_host = 'localhost';
+	global $mysql_user = 'root';
+	global $mysql_pass = '';
+	global $mysql_data = 'ab_testing';
 
 	global $connection;
 
-	if(!$connection = mysqli_connect($mysql_host , $mysql_user , $mysql_pass, $mysql_data))
-		die(mysqli_error($connection));
+	$options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
+	$connection = new PDO("mysql: host = $mysql_host; dbname = $mysql_data",$mysql_user,$mysql_pass,$options);
